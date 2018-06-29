13cc9697c89360ad4e3f08f443f1aa90c0b5b157
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2018-06-29 20:12:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# RegEx Matcher
+
+> _An object to simply test whether Strings match a regular expression or not and to extract specified components from the Strings._
+
+```
+// define a regular expression a String should match to and use opening and closing
+// parentheses to define the components to extract from the String
+String regularExpression = "^firstName:(.*), lastName:(.*)$";
+
+// define a String to test whether it matches to the regular expression and if so to
+// extract the specified components
+String myString = "firstName:Alex, lastName:Scheitlin";
+
+// test whether the String matches the regular expression and print its components
+// to the console
+RegexMatcher myRegexMatcher = new RegexMatcher(regularExpression);
+if (myRegexMatcher.doesMatch(myString)) {
+    try {
+        String[] components = myRegexMatcher.extractComponents(myString);
+        System.out.println("First Name: " + components[0]);
+        System.out.println("Last Name: " + components[1]);
+    } catch (Exception ex) {
+        System.out.println(ex.getMessage());
+    }
+}
+
+// silent way
+// check before matching
+if (myRegexMatcher.doesMatch(myString)) {
+    String[] components = myRegexMatcher.extractComponentsSilently(myString);
+    System.out.println("First Name: " + components[0]);
+    System.out.println("Last Name: " + components[1]);
+}
+```
---
+++ src/main/java/ch/scheitlin/alex/utils/RegexMatcher.java
@@ -0,0 +1,90 @@
+package ch.scheitlin.alex.utils;
+
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Represents a regular expression and provides functions to:
+ * - check whether a String matches the regular expression
+ * - extract the specified components of the regular expression
+ */
+public class RegexMatcher {
+    /**
+     * The regular expression as a String.
+     */
+    private String regex;
+
+    /**
+     * Creates a new instance of RegexMatcher.
+     *
+     * @param regex a regular expression
+     */
+    public RegexMatcher(String regex) {
+        this.regex = regex;
+    }
+
+    /**
+     * Checks whether a String matches the regular expression or not.
+     *
+     * @param string the String to test
+     * @return {@code true} if the String matches the regular expression,
+     * {@code false} if the String does not match the regular expression
+     */
+    public boolean doesMatch(String string) {
+        return string.matches(regex);
+    }
+
+    /**
+     * Matches the pattern to a String and returns the specified components of the String.
+     *
+     * @param string the String to extract the components from
+     * @return the specified components of the String
+     * @throws Exception if the String does not match the regular expression
+     */
+    public String[] extractComponents(String string) throws Exception {
+        String[] components = this.extract(string);
+        if (components == null) {
+            throw new Exception("Could not match the String to the provided pattern.");
+        } else {
+            return components;
+        }
+    }
+
+    /**
+     * Matches the pattern to a String and returns the specified components of the String.
+     *
+     * @param string the String to extract the components from
+     * @return the specified components of the String or {@code null} if the String does not
+     * match the regular expression
+     */
+    public String[] extractComponentsSilently(String string) {
+        return this.extract(string);
+    }
+
+    /**
+     * Matches the pattern to a String and returns the specified components of the String.
+     *
+     * @param string the String to extract the components from
+     * @return the specified components of the String
+     */
+    private String[] extract(String string) {
+        // match the pattern to the String
+        Pattern p = Pattern.compile(this.regex);
+        Matcher m = p.matcher(string);
+
+        // check whether the pattern matches the String
+        if (!m.matches()) {
+            return null;
+        }
+
+        // get matched components
+        MatchResult result = m.toMatchResult();
+        String[] components = new String[result.groupCount()];
+        for (int i = 0; i < result.groupCount(); i++) {
+            components[i] = result.group(i + 1);
+        }
+
+        return components;
+    }
+}
